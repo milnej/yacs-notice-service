3af32bd9ed32b2e8a7903243aa81244bbcd5acbf
milnej <milnej@rpi.edu>
2018-05-21 12:34:05 -0400

    migration null: false

--- db/migrate/20180521162002_create_notices.rb
+++ db/migrate/20180521162002_create_notices.rb
@@ -1,12 +1,12 @@
 class CreateNotices < ActiveRecord::Migration[5.2]
   def change
     create_table :notices do |t|
-      t.text :message
-      t.string :type
-      t.datetime :start_date
-      t.datetime :end_date
+      t.text :message           null: false
+      t.string :type            null: false
+      t.datetime :start_date    null: false
+      t.datetime :end_date      null: false
 
-      t.timestamps
+      t.timestamps              null: false
     end
   end
 end
